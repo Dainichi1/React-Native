83d3cb9bbfa091c1a3c007db1bdbbbc63dcac213
Dainichi1 <torquati79@yahoo.it>
2025-03-18 15:57:28 +0100

    31 - Making Items Deletable Using IDs

--- 02 - React Native Basics COURSE GOALS APP/my-app/App.js
+++ 02 - React Native Basics COURSE GOALS APP/my-app/App.js
@@ -9,11 +9,15 @@ export default function App() {
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      { text: enteredGoalText, key: Math.random().toString() },
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
   }
 
-  function deleteGoalHandler() {}
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) =>
+      currentCourseGoals.filter((goal) => goal.id !== id)
+    );
+  }
 
   return (
     <View style={styles.appContainer}>
@@ -25,6 +29,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
