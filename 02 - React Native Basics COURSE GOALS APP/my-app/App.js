163f55ab867ce6877107151dc5c52b76287e34cf
Dainichi1 <torquati79@yahoo.it>
2025-03-18 11:24:44 +0100

    17 - Exploring Layouts Flexbox

--- 02 - React Native Basics COURSE GOALS APP/my-app/App.js
+++ 02 - React Native Basics COURSE GOALS APP/my-app/App.js
@@ -1,28 +1,21 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>TEXT 2</Text>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add goal" />
       </View>
-      <Text style={styles.dummyText}>Hello World!</Text>
-      <Button title="Tap me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    padding: 16,
-    borderColor: "blue",
+  appContainer: {
+    padding: 50,
   },
 });
