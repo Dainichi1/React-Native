a4df8b222d97c8032d24b79556fe72cedc254c62
Dainichi1 <torquati79@yahoo.it>
2025-03-18 15:27:57 +0100

    27 - Splitting Components Into Smaller Components

--- 02 - React Native Basics COURSE GOALS APP/my-app/App.js
+++ 02 - React Native Basics COURSE GOALS APP/my-app/App.js
@@ -1,13 +1,6 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, Button, TextInput, FlatList } from "react-native";
 import { useState } from "react";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -38,11 +31,7 @@ export default function App() {
         <FlatList
           data={courseGoal}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem />;
           }}
           alwaysBounceVertical={false}
         />
@@ -78,15 +67,5 @@ const styles = StyleSheet.create({
 
   goalsContainer: {
     flex: 5,
-  },
-
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 5,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
