77fab365aaaef9a625f35b31e631c95ccf0a0a3c
Dainichi1 <torquati79@yahoo.it>
2025-03-19 11:56:39 +0100

    58 - Handling User Input Showing an Alert Dialog

--- 04 - Diving Deeper into Components Layouts Styling Building a MiniGame App/my-app/screens/StartGameScreen.js
+++ 04 - Diving Deeper into Components Layouts Styling Building a MiniGame App/my-app/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
@@ -9,7 +9,27 @@ function StartGameScreen() {
     setEnteredNumber(eneteredText);
   }
 
-  function confirmInputHandler() {}
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(eneteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid Number!",
+        "Number has to be a number between 1 and 99.",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: resetInputHandler,
+          },
+        ]
+      );
+      return;
+    }
+  }
 
   return (
     <View style={styles.inputContainer}>
@@ -24,7 +44,7 @@ function StartGameScreen() {
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
