895329bcd25aa7d4da186cedce8793e5be59013e
Dainichi1 <torquati79@yahoo.it>
2025-03-19 12:34:28 +0100

    63 - Managing Colors Globally

--- 04 - Diving Deeper into Components Layouts Styling Building a MiniGame App/my-app/screens/StartGameScreen.js
+++ 04 - Diving Deeper into Components Layouts Styling Building a MiniGame App/my-app/screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
+import Colors from "../constants/color";
 
 function StartGameScreen({ onPickNumber }) {
   const [eneteredNumber, setEnteredNumber] = useState("");
@@ -65,8 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
-    borderRadius: 8,
+    backgroundColor: Colors.primary800,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
@@ -78,9 +78,9 @@ const styles = StyleSheet.create({
     height: 60,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
