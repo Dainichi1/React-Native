8b3a0b09d697b2ccc531a40931e4d1cd5676bd36
Dainichi1 <torquati79@yahoo.it>
2025-03-19 10:02:01 +0100

    47 - Setting Up our Screen Components

---
+++ 04 - Diving Deeper into Components Layouts Styling Building a MiniGame App/my-app/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View, Button } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <Button title="START GAME" />
+    </View>
+  );
+}
+
+export default StartGameScreen;
