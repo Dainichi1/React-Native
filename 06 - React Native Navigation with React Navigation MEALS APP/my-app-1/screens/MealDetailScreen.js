66f3d326285056da3eb3d1e568cac9e0c3c73cd9
Dainichi1 <torquati79@yahoo.it>
2025-03-20 17:37:26 +0100

    107 - Adding an Icon Button to a Header

--- 06 - React Native Navigation with React Navigation MEALS APP/my-app-1/screens/MealDetailScreen.js
+++ 06 - React Native Navigation with React Navigation MEALS APP/my-app-1/screens/MealDetailScreen.js
@@ -1,16 +1,10 @@
-import {
-  Text,
-  View,
-  Button,
-  StyleSheet,
-  Image,
-  ScrollView,
-} from "react-native";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -22,7 +16,13 @@ function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Tap me!" onPress={headerButtonPressHandler} />;
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
       },
     });
   }, [navigation, headerButtonPressHandler]);
