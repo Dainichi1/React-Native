4eb4f9daaf0db03133ffb9f19bb0d229a810ff6c
Dainichi1 <torquati79@yahoo.it>
2025-03-21 09:20:39 +0100

    116 - Managing Appwide State with Context

--- 06 - React Native Navigation with React Navigation MEALS APP/my-app-1/store/context/favorites-context.js
+++ 06 - React Native Navigation with React Navigation MEALS APP/my-app-1/store/context/favorites-context.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 export const FavoritesContext = createContext({
   ids: [],
@@ -7,7 +8,29 @@ export const FavoritesContext = createContext({
 });
 
 function FavoriteContextProvider({ children }) {
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  const [favoriteMealsIds, setFavoriteMealsIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealsIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealsIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealsIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
 
 export default FavoriteContextProvider;
