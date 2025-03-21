a43a5a1bebf0ee1cfd12bb352e416920a0857ec0
Dainichi1 <torquati79@yahoo.it>
2025-03-21 18:25:09 +0100

    145 - Finishing Touches

--- 08 - Time To Practice The Expense Tracker App/my-app/components/ExpensesOutput/ExpensesOutput.js
+++ 08 - Time To Practice The Expense Tracker App/my-app/components/ExpensesOutput/ExpensesOutput.js
@@ -1,14 +1,19 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>; // ✅ Racchiuso in <Text>
+
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-
-      <ExpensesList expenses={expenses} />
+      {content} {/* ✅ Ora il fallbackText sarà sempre dentro un <Text> */}
     </View>
   );
 }
@@ -20,5 +25,12 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
