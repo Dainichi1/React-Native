fdd866e7772cef9e5160597f68a2cc7a83fd7700
Dainichi1 <torquati79@yahoo.it>
2025-03-22 15:31:09 +0100

    164 - Fetching Backend Data GET Requests

--- 08 - Time To Practice The Expense Tracker App/my-app/util/http.js
+++ 08 - Time To Practice The Expense Tracker App/my-app/util/http.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
+const BACKEND_URL =
+  "https://sezione-10-react-native-default-rtdb.firebaseio.com";
+
 export function storeExpense(expenseData) {
-  !axios.post(
-    "https://sezione-10-react-native-default-rtdb.firebaseio.com/expenses.json",
-    expenseData
-  );
+  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+}
+
+export async function fetchExpense() {
+  const response = await axios.post(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      data: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
