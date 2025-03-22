0fe033b41339568dce2e75d411984003d6b4ed74
Dainichi1 <torquati79@yahoo.it>
2025-03-22 10:24:25 +0100

    150 - Adding Styling

--- 08 - Time To Practice The Expense Tracker App/my-app/components/ManageExpense/Input.js
+++ 08 - Time To Practice The Expense Tracker App/my-app/components/ManageExpense/Input.js
@@ -1,12 +1,45 @@
-import { TextInput, Text, View } from "react-native";
+import { TextInput, Text, View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function Input({ label, textInputConfig }) {
+  const inputStyle = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyle.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput {...textInputConfig} style={inputStyle} />
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.gray700,
+  },
+
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
