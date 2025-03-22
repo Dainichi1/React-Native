17afc1aa4d4fcb6a7bca2b7f11abbbcd792fe155
Dainichi1 <torquati79@yahoo.it>
2025-03-22 16:00:07 +0100

    166 - Using Response Data from POST Requests

--- 08 - Time To Practice The Expense Tracker App/my-app/util/http.js
+++ 08 - Time To Practice The Expense Tracker App/my-app/util/http.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const BACKEND_URL =
   "https://sezione-10-react-native-default-rtdb.firebaseio.com";
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+export async function storeExpense(expenseData) {
+  const response = axios.post(BACKEND_URL + "/expenses.json", expenseData);
+  const id = response.data.name;
+  return id;
 }
 
 export async function fetchExpense() {
