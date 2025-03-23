96fb7732d4fb1813408e905c043f7337621f12f1
Dainichi1 <torquati79@yahoo.it>
2025-03-23 12:22:23 +0100

    178 - Logging Users In

--- 11 - User Authentication/my-app/util/auth.js
+++ 11 - User Authentication/my-app/util/auth.js
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBkN8zaWRbw-o_YAbU_u3DON7jMEYTwOmU";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signup", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
