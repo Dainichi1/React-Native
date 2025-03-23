cbb112c73a31151d7adfe8406d0ba6935fa4ee27
Dainichi1 <torquati79@yahoo.it>
2025-03-23 12:45:06 +0100

    181 - Extracting the Authentication Token

--- 11 - User Authentication/my-app/util/auth.js
+++ 11 - User Authentication/my-app/util/auth.js
@@ -10,23 +10,15 @@ async function authenticate(mode, email, password) {
     password: password,
     returnSecureToken: true,
   });
-  async function authenticate(mode, email, password) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
-    const response = await axios.post(url, {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
-
-    return response.data; // <--- fondamentale!
-  }
+  const token = response.data.idToken;
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signup", email, password);
+export function createUser(email, password) {
+  return authenticate("signup", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
