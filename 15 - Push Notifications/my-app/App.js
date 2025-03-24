3efad01742012ea41d661b94a80f1fd30ee5dcfe
Dainichi1 <torquati79@yahoo.it>
2025-03-24 12:48:13 +0100

    252 - Push Notifications Setup

--- 15 - Push Notifications/my-app/App.js
+++ 15 - Push Notifications/my-app/App.js
@@ -15,6 +15,12 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
+    Notifications.getExpoPushTokenAsync().then((pushTokenData) => {
+      console.log(pushTokenData); // oppure console.log(pushTokenData.data)
+    });
+  }, []);
+
+  useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
         console.log("NOTIFICATION RECEIVED");
